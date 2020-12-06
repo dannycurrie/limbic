727c80873f8f699efa691890b9073180144786ee
dannycurrie <danny.currie42@gmail.com>
2020-12-06 21:00:46 +0000

    initial micro frontend test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'limbic';
+
+  host = 'http://localhost:8080/main.js';
+  
+  ngOnInit(): void {
+    const script = document.createElement('script');
+    script.id = 'test';
+    script.crossOrigin = '';
+    script.src = this.host;
+    // append the app entry app element to this app's template
+    script.onload = () => console.log('hello!');
+    document.firstElementChild.appendChild(script);
+  }
 }
